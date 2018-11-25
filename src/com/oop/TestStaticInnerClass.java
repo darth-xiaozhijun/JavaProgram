4f9c0974948a7213d7a1c5d2692581f8a1a30882
xiaozhijun1027 <15717061273@163.com>
2018-11-25 17:42:57 +0800

    静态内部类的学习

--- src/com/oop/TestStaticInnerClass.java
+++ src/com/oop/TestStaticInnerClass.java
@@ -1,19 +1,14 @@
 package com.oop;
 
 /**
- * 测试内部类
+ * 测试静态内部类
  * @author Administrator
  *
  */
-public class TestInnerClass {
-	
-	public static void main(String[] args) {
-		
-		//实例化内部类对象
-		Outer.Inner inner = new Outer().new Inner();
-		
-		inner.show();
-	}
+public class TestStaticInnerClass {
+
+	//实例化静态内部类
+	Outer2.Inner2 inner2 = new Outer2.Inner2();
 }
 
 /**
@@ -21,16 +16,16 @@ public class TestInnerClass {
  * @author Administrator
  *
  */
-class Outer{
+class Outer2 {
 	
 	int age = 10;
 	
 	/**
-	 * 内部类
+	 * 静态内部类
 	 * @author Administrator
 	 *
 	 */
-	class Inner{
+	static class Inner2 {
 		
 		int age = 20;
 		
@@ -38,10 +33,10 @@ class Outer{
 			
 			int age = 30;
 			
-			System.out.println("外部类的成员变量：" + Outer.this.age);
+//			System.out.println("外部类的成员变量：" + Outer2.this.age);//不能访问外部类的成员变量
 			System.out.println("内部类的成员变量：" + this.age);
 			System.out.println("内部类的局部变量：" + age);
 		}
 	}
-	
 }
+
