60c62f5423779aeef3dda2b45d1a091f1fd8ff39
xiaozhijun1027 <15717061273@163.com>
2018-11-28 22:52:46 +0800

    冒泡排序

---
+++ src/com/arrays/TestBubbleSort.java
@@ -0,0 +1,39 @@
+package com.arrays;
+
+import java.util.Arrays;
+
+/**
+ * 冒泡排序
+ * @author Administrator
+ *
+ */
+public class TestBubbleSort {
+
+	public static void main(String[] args) {
+		
+		int[] values = {3,1,6,2,9,0,7,4,5,8};
+		int temp = 0;
+		
+		for(int i=0; i<values.length-1; i++){
+			
+			boolean isExchange = true;
+			for(int j=0; j<values.length-1-i; j++){
+				
+				if(values[j] > values[j+1]){
+					temp = values[j+1];
+					values[j+1] = values[j];
+					values[j] = temp;
+					
+					isExchange = false;
+				}
+				
+				System.out.println(Arrays.toString(values));
+			}
+			
+			System.out.println("第"+(i+1)+"趟排序");
+			if(isExchange){
+				break;
+			}
+		}
+	}
+}
