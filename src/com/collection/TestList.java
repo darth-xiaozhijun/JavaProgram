63d64895587c709cad9f0e134e5155f5dc85f09e
xiaozhijun1027 <15717061273@163.com>
2018-12-08 00:16:31 +0800

    List的相关方法

--- src/com/collection/TestList.java
+++ src/com/collection/TestList.java
@@ -2,6 +2,7 @@ package com.collection;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /*
  * 测试Collection中list集合
@@ -9,6 +10,37 @@ import java.util.Collection;
 public class TestList {
 
 	public static void main(String[] args) {
+		
+		test2();
+		
+	}
+	
+	public static void test2(){
+		
+		List<String> list1 = new ArrayList<>();
+		list1.add("aa");
+		list1.add("bb");
+		list1.add("cc");
+		
+		List<String> list2 = new ArrayList<>();
+		list2.add("aa");
+		list2.add("dd");
+		list2.add("ee");
+		
+		System.out.println("list1: " + list1);
+		
+//		list1.addAll(list2);//把list2中的所有元素添加到list1
+		
+//		list1.removeAll(list2);//删除和list2中相同的元素
+		
+		list1.retainAll(list2);//取交集
+		
+		System.out.println("list1: " + list1);
+		
+		System.out.println("list1中是否全部包含list2中的元素：" + list1.containsAll(list2));
+	}
+	
+	public static void test1(){
 		
 		Collection<String> collection = new ArrayList<>();
 		
@@ -30,6 +62,5 @@ public class TestList {
 		collection.clear();
 		System.out.println(collection);
 		System.out.println(collection.size());
-		
 	}
 }
