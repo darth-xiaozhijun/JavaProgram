429cbcd7a497ca9ba5b1492a7d82e970625aa2a1
xiaozhijun1027 <15717061273@163.com>
2018-12-11 23:54:38 +0800

    测试Vector，线程安全

---
+++ src/com/collection/TestVector.java
@@ -0,0 +1,14 @@
+package com.collection;
+
+import java.util.List;
+import java.util.Vector;
+
+public class TestVector {
+
+	public static void main(String[] args) {
+		
+		List<String> vector = new Vector<>();
+		vector.add("aa");
+		System.out.println(vector);
+	}
+}
