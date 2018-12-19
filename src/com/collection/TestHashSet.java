15a8af02c36359173ef1979e50a430de199070a6
xiaozhijun1027 <15717061273@163.com>
2018-12-19 22:40:07 +0800

    测试hashset的基本用法

---
+++ src/com/collection/TestHashSet.java
@@ -0,0 +1,25 @@
+package com.collection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestHashSet {
+
+	public static void main(String[] args) {
+		
+		Set<String>  set1 = new HashSet<>();
+		
+		set1.add("aa");
+		set1.add("bb");
+		set1.add("aa");
+		System.out.println(set1);
+		set1.remove("bb");
+		System.out.println(set1);
+		
+	
+		Set<String>  set2 = new HashSet<>();
+		set2.add("高淇");
+		set2.addAll(set1);
+		System.out.println(set2);
+	}
+}
