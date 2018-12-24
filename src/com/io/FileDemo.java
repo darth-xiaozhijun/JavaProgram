6add48337eb0c18d5808053a2e903cc2726cbbc4
xiaozhijun1027 <15717061273@163.com>
2018-12-24 22:41:21 +0800

    相对路径和绝对路径

---
+++ src/com/io/FileDemo.java
@@ -0,0 +1,23 @@
+package com.io;
+
+import java.io.File;
+
+public class FileDemo {
+
+	public static void main(String[] args) {
+		
+		String path = "D:/workspace/xiaozhijun/JavaProgram/src/com/io/IO.png";
+		
+		File src = new File(path);
+		System.out.println(src.length());
+		
+		src = new File("D:/workspace/xiaozhijun/JavaProgram/src/com/io/", "IO.png");
+		System.out.println(src.length());
+		
+		src = new File("D:/workspace/xiaozhijun/JavaProgram/src/com/","io/IO.png");
+		System.out.println(src.length());
+		
+		src = new File(new File("D:/workspace/xiaozhijun/JavaProgram/src/com/io/"), "IO.png");
+		System.out.println(src.length());
+	}
+}
