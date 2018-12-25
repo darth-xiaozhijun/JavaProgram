2c5e4212a733ce80127ef3d894e8f80974908eea
xiaozhijun1027 <15717061273@163.com>
2018-12-25 21:45:52 +0800

    判断是否存在，是否是文件、文件夹

---
+++ src/com/io/FileDemo4.java
@@ -0,0 +1,16 @@
+package com.io;
+
+import java.io.File;
+
+public class FileDemo4 {
+
+	public static void main(String[] args) {
+		
+		String path = "D:/workspace/xiaozhijun/JavaProgram/src/com/io/IO.png";
+		
+		File src = new File(path);
+		System.out.println(src.exists());
+		System.out.println(src.isFile());
+		System.out.println(src.isDirectory());
+	}
+}
