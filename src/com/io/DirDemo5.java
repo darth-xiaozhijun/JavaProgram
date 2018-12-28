0f3ae0ca16927e687606f557e60059ddad617971
xiaozhijun1027 <15717061273@163.com>
2018-12-28 23:26:44 +0800

    递归获取文件夹大小

---
+++ src/com/io/DirDemo5.java
@@ -0,0 +1,33 @@
+package com.io;
+
+import java.io.File;
+
+public class DirDemo5 {
+	
+	private static Long allLenth = 0L;
+
+	public static void main(String[] args) {
+		
+		File file = new File("D:/workspace/xiaozhijun/JavaProgram");
+		getAllLength(file);
+		System.out.println(allLenth);
+	}
+	
+	public static Long getAllLength(File file){
+		
+		if(file != null && file.exists()){
+			
+			if(file.isFile()){
+				allLenth += file.length();
+			}else{
+				
+				for(File f : file.listFiles()){
+					
+					getAllLength(f);
+				}
+			}
+		}
+		
+		return allLenth;
+	}
+}
