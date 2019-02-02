20448a1349cb818d5c2278f4eb60f2fb4bfacfa6
xiaozhijun1027 <15717061273@163.com>
2019-02-02 16:28:53 +0800

    编码与解码

---
+++ src/com/io/ContentEncode.java
@@ -0,0 +1,29 @@
+package com.io;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * 编码：字符串-->字节
+ * @author Administrator
+ *
+ */
+public class ContentEncode {
+	
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		
+		String msg = "性命生命使命a";
+		
+		//使用项目本身的编码格式，UTF-8，一个汉字三个字节
+		byte[] datas = msg.getBytes();
+		System.out.println(datas.length);
+		
+		//所有的都是两个字节
+		datas = msg.getBytes("UTF-16LE");
+		System.out.println(datas.length);
+		
+		//一个汉字两个字节
+		datas = msg.getBytes("GBK");
+		System.out.println(datas.length);
+	}
+
+}
