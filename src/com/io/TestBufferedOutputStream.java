6a8fc3d01c073bd8c2d034ce585e2157ebcbdf48
xiaozhijun1027 <15717061273@163.com>
2019-03-09 20:42:48 +0800

    字节缓冲流

---
+++ src/com/io/TestBufferedOutputStream.java
@@ -0,0 +1,57 @@
+package com.io;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * 字节输出流，加入缓冲流
+ * 1、创建源
+ * 2、选择流
+ * 3、操作
+ * 4、释放资源
+ * @author Administrator
+ */
+public class TestBufferedOutputStream {
+
+	public static void main(String[] args) {
+		
+		//创建源
+		File dest = new File("src/com/io/dest.txt");
+		
+		//选择流
+		OutputStream outputStream = null;
+		
+		//操作
+		try {
+			
+			outputStream = new BufferedOutputStream(new FileOutputStream(dest));
+			
+			String msg = "I am going to school!\r\n";
+			byte[] bs = msg.getBytes();
+			
+			outputStream.write(bs, 0, bs.length);
+			outputStream.flush();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}finally {
+			
+			//释放资源
+			if(null != outputStream){
+				try {
+					outputStream.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
