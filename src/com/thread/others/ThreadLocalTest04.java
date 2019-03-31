ae3d397331fc5f4090117fd37217f85621b7c25c
xiaozhijun1027 <15717061273@163.com>
2019-03-31 21:33:30 +0800

    ThreadLocal

---
+++ src/com/thread/others/ThreadLocalTest04.java
@@ -0,0 +1,22 @@
+package com.thread.others;
+/**
+ * InheritableThreadLocal:继承上下文 环境的数据 ，拷贝一份给子线程
+ *
+ */
+public class ThreadLocalTest04 {	
+	private static ThreadLocal<Integer> threadLocal = new InheritableThreadLocal<>();
+	public static void main(String[] args) {
+		threadLocal.set(2);
+		System.out.println(Thread.currentThread().getName()+"-->"+threadLocal.get());	
+		
+		//线程由main线程开辟
+		new Thread(()->{
+			System.out.println(Thread.currentThread().getName()+"-->"+threadLocal.get());	
+			threadLocal.set(200);
+			System.out.println(Thread.currentThread().getName()+"-->"+threadLocal.get());	
+		}) .start();
+		
+	}	
+	
+	
+}
