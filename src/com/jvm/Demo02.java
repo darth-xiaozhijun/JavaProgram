08e422651cf11221b6795d720d0b541d3dc7fb4e
xiaozhijun1027 <15717061273@163.com>
2019-05-08 20:12:38 +0800

    类加载器的层次结构

---
+++ src/com/jvm/Demo02.java
@@ -0,0 +1,17 @@
+package com.jvm;
+
+public class Demo02 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(ClassLoader.getSystemClassLoader());
+		System.out.println(ClassLoader.getSystemClassLoader().getParent());
+		System.out.println(ClassLoader.getSystemClassLoader().getParent().getParent());
+		System.out.println(System.getProperty("java.class.path"));
+		
+		System.out.println("################");
+		String a = "gaogao";
+		System.out.println(a.getClass().getClassLoader());
+		System.out.println(a);
+	}
+}
