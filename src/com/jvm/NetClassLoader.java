80521c04a2ba1dcc4634cd49042a79b1af85710f
xiaozhijun1027 <15717061273@163.com>
2019-05-08 21:08:41 +0800

    网络类加载器

--- src/com/jvm/NetClassLoader.java
+++ src/com/jvm/NetClassLoader.java
@@ -4,18 +4,18 @@ import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 
 /**
- * 自定义文件系统类加载器
- * @author Administrator
+ * 网络类加载器
  *
  */
-public class FileSystemClassLoader extends ClassLoader {
-
-	private String rootDir;
+public class NetClassLoader extends ClassLoader {
 	
-	public FileSystemClassLoader(String rootDir) {
-		this.rootDir = rootDir;
+	private String rootUrl;
+	
+	public NetClassLoader(String rootUrl){
+		this.rootUrl = rootUrl;
 	}
 	
 	@Override
@@ -24,42 +24,42 @@ public class FileSystemClassLoader extends ClassLoader {
 		Class<?> c = findLoadedClass(name);
 		
 		//应该要先查询有没有加载过这个类。如果已经加载，则直接返回加载好的类。如果没有，则加载新的类。
-		if(c != null){
+		if(c!=null){
 			return c;
-		}else {
-			
+		}else{
 			ClassLoader parent = this.getParent();
-			
 			try {
-				
-				c = parent.loadClass(name);//委派给父类加载
+				c = parent.loadClass(name);	   //委派给父类加载
 			} catch (Exception e) {
+//				e.printStackTrace();
 			}
 			
-			if(c != null){
+			if(c!=null){
 				return c;
-			}else {
-				
+			}else{
 				byte[] classData = getClassData(name);
-				if(classData == null){
+				if(classData==null){
 					throw new ClassNotFoundException();
 				}else{
-					c = defineClass(name, classData, 0, classData.length);
+					c = defineClass(name, classData, 0,classData.length);
 				}
 			}
 			
 		}
+		
 		return c;
+		
 	}
 	
 	private byte[] getClassData(String classname){   //com.bjsxt.test.User   d:/myjava/  com/bjsxt/test/User.class
-		String path = rootDir +"/"+ classname.replace('.', '/')+".class";
+		String path = rootUrl +"/"+ classname.replace('.', '/')+".class";
 		
-//		IOUtils,可以使用它将流中的数据转成字节数组
+//		IOUtils,����ʹ���������е�����ת���ֽ�����
 		InputStream is = null;
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		try{
-			is  = new FileInputStream(path);
+			URL url = new URL(path);
+			is  = url.openStream();
 			
 			byte[] buffer = new byte[1024];
 			int temp=0;
@@ -89,4 +89,8 @@ public class FileSystemClassLoader extends ClassLoader {
 		}
 		
 	}
+	
+	
+	
+	
 }
