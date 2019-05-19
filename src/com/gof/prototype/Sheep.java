89963c25cee1f76893f531bd307f070d595c6ea2
xiaozhijun1027 <15717061273@163.com>
2019-05-19 21:56:29 +0800

    原型模式(深复制,使用序列化和反序列化的方式实现深复制)

--- src/com/gof/prototype/Sheep.java
+++ src/com/gof/prototype/Sheep.java
@@ -1,8 +1,9 @@
 package com.gof.prototype;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class Sheep implements Cloneable{
+public class Sheep implements Cloneable,Serializable{
 
 	private String name;
 	
