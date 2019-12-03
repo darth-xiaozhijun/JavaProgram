35f00048955ad3360ce54e5fc5f53ed49b68e251
darth-xiaozhijun <15717061273@163.com>
2019-12-03 17:43:02 +0800

    Thread类本质上是实现了Runnable接口的一个实例，代表一个线程的实例

--- src/com/thread/StartThread.java
+++ src/com/thread/StartThread.java
@@ -4,6 +4,10 @@ package com.thread;
  * 创建线程方式之一：
  * 1、创建：继承Thread+重写run方法
  * 2、启动:创建子类对象+start
+ * 
+ * Thread类本质上是实现了Runnable接口的一个实例，代表一个线程的实例。
+ * 启动线程的唯一方法就是通过Thread类的start()实例方法。
+ * start()方法是一个native方法，它将启动一个新线程，并执行run()方法。
  * @author Administrator
  *
  */
